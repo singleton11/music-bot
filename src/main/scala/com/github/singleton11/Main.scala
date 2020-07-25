e7ceff88765ce3269508a35bb6b4a4c534404a6d
Anton Prokhorov <betrayer11@gmail.com>
2020-07-26 00:50:26 +0300

    Rewrite Main.run to make more pure

--- src/main/scala/com/github/singleton11/Main.scala
+++ src/main/scala/com/github/singleton11/Main.scala
@@ -6,10 +6,13 @@ import com.github.singleton11.repository.CurrentTrackRepository
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    new CurrentTrackRepository[IO](CoreRadioCurrentTrackAlgebra.IOInterpreter(implicitly[ConcurrentEffect[IO]])).get
-      .map(value => println(value))
-      .unsafeRunSync()
+    for {
+      currentTrack <- getCurrentTrackRepository.get
+      _ <- IO(println(currentTrack))
+    } yield ExitCode.Success
+  }
 
-    IO.pure(ExitCode.Success)
+  private def getCurrentTrackRepository = {
+    new CurrentTrackRepository[IO](CoreRadioCurrentTrackAlgebra.IOInterpreter(implicitly[ConcurrentEffect[IO]]))
   }
 }
