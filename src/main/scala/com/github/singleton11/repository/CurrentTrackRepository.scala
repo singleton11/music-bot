f42a13265a3698517abb19245286acc023712711
Anton Prokhorov <betrayer11@gmail.com>
2020-07-26 00:02:52 +0300

    Add correct modifiers in CurrentTrackRepository

--- src/main/scala/com/github/singleton11/repository/CurrentTrackRepository.scala
+++ src/main/scala/com/github/singleton11/repository/CurrentTrackRepository.scala
@@ -9,21 +9,21 @@ import com.github.singleton11.util.implicits._
 class CurrentTrackRepository[F[_]](algebra: CurrentTrackAlgebra[F, String])(implicit m: Monad[F]) {
   def get: F[Option[CurrentTrack]] = m.map(algebra.getCurrentTrack)(value => getCurrentTrackFromHtml(value))
 
-  def getRawTrackValue(html: String): Option[String] = {
+  private def getRawTrackValue(html: String): Option[String] = {
     for {
       start <- html indexAfter "<title>"
       end <- html indexBefore "</title>"
     } yield html.slice(start, end)
   }
 
-  def getCurrentTrack(rawTrackValue: String): Option[CurrentTrack] = {
+  private def getCurrentTrack(rawTrackValue: String): Option[CurrentTrack] = {
     for {
       track <- (rawTrackValue indexBefore " - ").map(value => rawTrackValue.slice(0, value))
       artist <- (rawTrackValue indexAfter " - ").map(value => rawTrackValue.slice(value, rawTrackValue.length))
     } yield CurrentTrack(track, artist)
   }
 
-  def getCurrentTrackFromHtml(html: String): Option[CurrentTrack] = {
+  private def getCurrentTrackFromHtml(html: String): Option[CurrentTrack] = {
     for {
       rawTrackValue <- getRawTrackValue(html)
       currentTrack <- getCurrentTrack(rawTrackValue)
