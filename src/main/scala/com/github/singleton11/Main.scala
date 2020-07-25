46268b245a2ffc4f0dde512fdf8ef45a77db2fa3
Anton Prokhorov <betrayer11@gmail.com>
2020-07-26 00:54:54 +0300

    Make getCurrentTrackRepository property

--- src/main/scala/com/github/singleton11/Main.scala
+++ src/main/scala/com/github/singleton11/Main.scala
@@ -5,14 +5,13 @@ import com.github.singleton11.interpreter.CoreRadioCurrentTrackAlgebra
 import com.github.singleton11.repository.CurrentTrackRepository
 
 object Main extends IOApp {
+
+  private val currentTrackRepository = new CurrentTrackRepository[IO](CoreRadioCurrentTrackAlgebra.IOInterpreter(implicitly[ConcurrentEffect[IO]]))
+
   override def run(args: List[String]): IO[ExitCode] = {
     for {
-      currentTrack <- getCurrentTrackRepository.get
+      currentTrack <- currentTrackRepository.get
       _ <- IO(println(currentTrack))
     } yield ExitCode.Success
-  }
-
-  private def getCurrentTrackRepository = {
-    new CurrentTrackRepository[IO](CoreRadioCurrentTrackAlgebra.IOInterpreter(implicitly[ConcurrentEffect[IO]]))
   }
 }
